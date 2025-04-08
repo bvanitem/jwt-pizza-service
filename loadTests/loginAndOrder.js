3d0e6726f1b69673f923b8ec34021e7ff9bf77a1
bvanitem <blainevanitem2000@gmail.com>
2025-04-08 00:37:52 -0600

    making sure push to ec is correct, am unable to curl service?

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,248 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 2, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  group('page_1 - https://pizza.bvanitem.com/', function () {
+    response = http.get('https://pizza.bvanitem.com/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Tue, 01 Apr 2025 17:58:25 GMT',
+        'if-none-match': '"b0d90491da1dbf9021b377258fd6c3fb"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(10.3)
+
+    response = http.put(
+      'https://pizza-service.cs329.click/api/auth',
+      '{"email":"a@jwt.com","password":"admin"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.bvanitem.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+    check(response, { 'status equals 200': response => response.status.toString() === '200' })
+
+    vars['token1'] = jsonpath.query(response.json(), '$.token')[0]
+
+    response = http.options('https://pizza-service.cs329.click/api/auth', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'content-type',
+        'access-control-request-method': 'PUT',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+    sleep(2.8)
+
+    response = http.get('https://pizza-service.cs329.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    })
+
+    response = http.options('https://pizza-service.cs329.click/api/order/menu', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+
+    response = http.get('https://pizza-service.cs329.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token1']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"1ad-+pf1vS8S701z4ATQv9Fd6vXEWTo"',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-storage-access': 'active',
+      },
+    })
+
+    response = http.options('https://pizza-service.cs329.click/api/franchise', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'GET',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+    sleep(11.5)
+
+    response = http.post(
+      'https://pizza-service.cs329.click/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":2,"description":"Pepperoni","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.bvanitem.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+
+    response = http.options('https://pizza-service.cs329.click/api/order', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+    sleep(5.9)
+
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      '{"jwt":"eyJpYXQiOjE3NDM1MzExMTIsImV4cCI6MTc0MzYxNzUxMiwiaXNzIjoiY3MzMjkuY2xpY2siLCJhbGciOiJSUzI1NiIsImtpZCI6IjE0bk5YT21jaWt6emlWZWNIcWE1UmMzOENPM1BVSmJuT2MzazJJdEtDZlEifQ.eyJ2ZW5kb3IiOnsiaWQiOiJqd3QtaGVhZHF1YXJ0ZXJzIiwibmFtZSI6IkpXVCBIZWFkcXVhcnRlcnMifSwiZGluZXIiOnsiaWQiOjEsIm5hbWUiOiLluLjnlKjlkI3lrZciLCJlbWFpbCI6ImFAand0LmNvbSJ9LCJvcmRlciI6eyJpdGVtcyI6W3sibWVudUlkIjoxLCJkZXNjcmlwdGlvbiI6IlZlZ2dpZSIsInByaWNlIjowLjAwMzh9LHsibWVudUlkIjoyLCJkZXNjcmlwdGlvbiI6IlBlcHBlcm9uaSIsInByaWNlIjowLjAwNDJ9XSwic3RvcmVJZCI6IjEiLCJmcmFuY2hpc2VJZCI6MSwiaWQiOjkzfX0.P0llEeKD0-r7gUCvO-aEjfp136lXzqvxPxAQFGrKtoOmIPzFDmoqLTo1JDwGK2ipcYFufxSpQVehg6JICv2kuoD78BLub6BiXPBJ-C9EkvqIs0DkCsn_FxATiT-bxTCgEtpFM1I_pdaydE0D0PneM5xw1Crk8CTo_f42bgVzCuDh4lKUn4pyZrv-SNTqx4BoSmO08zhM2EpQmJKfCTeYa_zJ2GIhzOohoz9gQBPsieggAqWb2rEdeFirgdu5ab32S5Fhc0m6x3hyF689RQnclALgNIRrbxb3pYYT2Xm0ui-SiW8Kl42XWG2pN947BoG9EONbzp0AP3frORrGFpnDOmFqQQ6eAygTOzrNqmJsMcmeDnyHH66QpjrtlkDS5izRmxzPUxGdLSAdfb-v4K2d18bxgcEBDSUo8Cfc81zEBuVnRzkqfExtn6O2ne74LRje3DLAATugqJLszR7-VvR2jjWZcKb-y51tsNbWxn8M2YGcIW8AuSbDY5LMuu0ILFSgXbxR5fE4WIb8ht_J84f2q0OixeiYpaYGfZOHs4l5oZ4xVcNyYODEhIaVWb_tiuVC4NgRcqNBTR9RgHBnrbhtq9DmS_KK8YupkHAVXelFy1n_zSFBGiszlUuhXNknHh3RGXbuiGm-H2drwOwz-MvIrqiFFADJw8GF3ouwnTirOtQ"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token1']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.bvanitem.com',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+
+    response = http.options('https://pizza-factory.cs329.click/api/order/verify', null, {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'access-control-request-headers': 'authorization,content-type',
+        'access-control-request-method': 'POST',
+        origin: 'https://pizza.bvanitem.com',
+        priority: 'u=1, i',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+      },
+    })
+      if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+    console.log(response.body);
+    fail('Login was *not* 200');
+  }
+  })
+}
